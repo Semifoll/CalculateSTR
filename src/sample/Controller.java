fef4a6227fe2f9ce7c113061a1cce2bec5b29175
Semifoll <poqw_1212>
2018-04-03 16:09:55 +0300

    general commit
     add style in SceneBuilder to fxml

---
+++ src/sample/Controller.java
@@ -0,0 +1,145 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller {
+
+    @FXML
+    private Tab simpleTab_Button;
+
+    @FXML
+    private AnchorPane simpleTab;
+
+    @FXML
+    private Button sTab_Button_1;
+
+    @FXML
+    private Button sTab_Button_2;
+
+    @FXML
+    private Button sTab_Button_Plus;
+
+    @FXML
+    private Button sTab_Button_Minus;
+
+    @FXML
+    private TextField sTabTextField;
+
+    @FXML
+    private Button sTab_Button_3;
+
+    @FXML
+    private Button sTab_Button_4;
+
+    @FXML
+    private Button sTab_Button_5;
+
+    @FXML
+    private Button sTab_Button_6;
+
+    @FXML
+    private Button sTab_Button_7;
+
+    @FXML
+    private Button sTab_Button_8;
+
+    @FXML
+    private Button sTab_Button_9;
+
+    @FXML
+    private Button sTab_Button_0;
+
+    @FXML
+    private Button sTab_Button_Multiply;
+
+    @FXML
+    private Button sTab_Button_Divide;
+
+    @FXML
+    private Button sTab_Button_Equally;
+
+    @FXML
+    private Button sTab_Button_Comma;
+
+    @FXML
+    private ListView<?> sTab_Button_List;
+
+    @FXML
+    private Button sTab_Button_Delete;
+
+    @FXML
+    private Tab expressionTab_Button;
+
+    @FXML
+    private AnchorPane expressionTab;
+
+    @FXML
+    private Button eTab_Button_Plus;
+
+    @FXML
+    private Button eTab_Button_Minus;
+
+    @FXML
+    private TextField eTab_Button_Text_Field;
+
+    @FXML
+    private Button eTab_Button_3;
+
+    @FXML
+    private Button eTab_Button_6;
+
+    @FXML
+    private Button eTab_Button_8;
+
+    @FXML
+    private Button eTab_Button_Multiply;
+
+    @FXML
+    private Button eTab_Button_Divide;
+
+    @FXML
+    private Button eTab_Button_Equally;
+
+    @FXML
+    private Button eTab_Button_Open;
+
+    @FXML
+    private Button eTab_Button_Close;
+
+    @FXML
+    private Button eTab_Button_1;
+
+    @FXML
+    private Button eTab_Button_2;
+
+    @FXML
+    private Button eTab_Button_4;
+
+    @FXML
+    private Button eTab_Button_5;
+
+    @FXML
+    private Button eTab_Button_7;
+
+    @FXML
+    private Button eTab_Button_0;
+
+    @FXML
+    private Button eTab_Button_Comma;
+
+    @FXML
+    private Button eTab_Button_Cancel;
+
+    @FXML
+    private Button eTab_Button_Delete;
+
+    @FXML
+    private ListView<?> eTab_Button_List;
+
+}
+
